eea739d676dc59d8a50002159e9609240bc3c05e
inventedbeast29 <kishanrai18739@gmail.com>
2025-10-04 22:02:31 +0530

    Map Initalised

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require("express")
+const Socket=require("socket.io");
+const http=require("http");
+
+
+
+const app=express();
+app.set("view engine","ejs");
+const Server=http.createServer(app);
+
+
+const io = Socket(Server);
+
+
+app.get("/",(req,res)=>{
+    res.render("map")
+})
+
+io.on("connection",(socket)=>{
+    console.log("Device connected",socket.id);
+    
+})
+
+Server.listen("1111",()=>{
+    console.log("server started")
+});
