ec0ab547d8c946429534e6554dd0267fa3f9e3ad
inventedbeast29 <kishanrai18739@gmail.com>
2025-11-11 20:34:59 +0530

    Geo fencing added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const Socket=require("socket.io");
 const http=require("http");
+const geolib=require("geolib");
 
 
 
@@ -9,18 +10,24 @@ app.set("view engine","ejs");
 const Server=http.createServer(app);
 
 
-const io = Socket(Server);
 
+const fence={latitude:28.44654213710573,longitude:77.07210118837843} 
+const io = Socket(Server);
 
 app.get("/",(req,res)=>{
     res.render("map")
 })
 
+
 io.on("connection",(socket)=>{
     console.log("Device connected",socket.id);
     socket.on("position",(data)=>{
        const {latitude,longitude}=data;
-        socket.broadcast.emit("pos", { latitude, longitude });
+       const userLocation={latitude,longitude};      
+        const inside= geolib.isPointWithinRadius(userLocation, fence, 1000);
+       // console.log("isInside",inside);
+            // console.log(latitude,longitude); 28.4464711 77.0720595
+        io.emit("pos", { latitude, longitude,inside });
     })
 })
 
